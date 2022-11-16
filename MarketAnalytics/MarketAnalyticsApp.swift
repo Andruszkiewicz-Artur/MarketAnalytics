ab261eb70824da1173f21be5fc2139b0c2f92d06
Artur Andruszkiewicz <andruszkiewiczarturmobiledev@gmail.com>
2022-11-16 17:06:52 +0100

    first commit

---
+++ MarketAnalytics/MarketAnalyticsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarketAnalyticsApp.swift
+//  MarketAnalytics
+//
+//  Created by Artur Andruszkiewicz on 16/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct MarketAnalyticsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
